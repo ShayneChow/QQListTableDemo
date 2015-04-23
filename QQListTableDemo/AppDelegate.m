8a334372313c153f1fad9bd79a3afefb04637e70
Shayne Chow <shayne@authbuy.org>
2015-04-24 00:44:47 +0800

    创建项目，并搭建基本框架。

--- QQListTableDemo/AppDelegate.m
+++ QQListTableDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ListTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    ListTableViewController *listTableView = [[ListTableViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:listTableView];
+    self.window.rootViewController = nav;
+    
     return YES;
 }
 
--- QQListTableDemo/ListTableViewController.h
+++ QQListTableDemo/ListTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListTableViewController.h
 //  QQListTableDemo
 //
 //  Created by ChowShayne on 15/4/24.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListTableViewController : UITableViewController
 
 @end
-
